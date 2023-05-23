02b74566377168d10ebeebe9b72f78ff10ebf1f2
Mathieu Dourlens <matdourlens95@gmail.com>
2023-05-23 17:58:50 +0200

    Compilation du JS et Redéfinition des images (redimension , compression , convertion webp pour les JPEG

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -229,6 +229,22 @@
             .show(300);
         }
       });
-    }
+    }  
   };
-})(jQuery);
+  $(document).ready(function() {
+    $('.gallery').mauGallery({
+        columns: {
+            xs: 1,
+            sm: 2,
+            md: 3,
+            lg: 3,
+            xl: 3
+        },
+        lightBox: true,
+        lightboxId: 'myAwesomeLightbox',
+        showTags: true,
+        tagsPosition: 'top'
+    });
+  });  
+})
+(jQuery);
